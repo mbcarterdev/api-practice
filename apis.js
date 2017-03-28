24874d754bace725d05465a7a2c9b733c4e3c695
Michelle Carter <embeesea@Michelles-MacBook-Pro.local>
2017-03-27 22:48:22 -0600

    Add gitignore and make character api variable

--- apis.js
+++ apis.js
@@ -2,16 +2,18 @@ import rp from 'request-promise';
 
 const apis = {
   fetchCharacter: (req, res) => {
-    rp.get(`https://swapi.co/api/people/?search=luke`)
+    console.log('params: ', req.params);
+    const name = req.params.name;
+    rp.get(`https://swapi.co/api/people/?search=${name}`)
       .then((data) => {
         const char = JSON.parse(data);
         console.log(char.results[0].name);
-        // const charName = data.results[0].name;
-        // const hair = data.results[0].hair_color;
-        // const eyes = data.results[0].eye_color;
-        // let characterString = charName + ' has ' + hair + ' hair and ' + eyes + ' eyes.';
-        // console.log(characterString);
-        res.status(200).send(char.results[0].name)
+        const charName = char.results[0].name;
+        const hair = char.results[0].hair_color;
+        const eyes = char.results[0].eye_color;
+        let characterString = charName + ' has ' + hair + ' hair and ' + eyes + ' eyes.';
+        console.log(characterString);
+        res.status(200).send(characterString);
       })
   }
 
--- index.js
+++ index.js
@@ -6,14 +6,14 @@ import apis from './apis.js';
 const app = express();
 const router = express.Router();
 
-app.use(parser.json());
 app.use(router);
+app.use(parser.json());
 
 router.get('/', function (req, res) {
   res.send('hello world');
 });
-// router.get('/character/:name', apis.fetchCharacter);
-router.get('/character', apis.fetchCharacter);
+
+router.get('/character/:name', apis.fetchCharacter);
 
 app.listen(8080, function () {
   console.log('Back-end API app is listening on port 8080')
